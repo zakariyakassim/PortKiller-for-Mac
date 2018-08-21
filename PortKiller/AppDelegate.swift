3d0868a29e79e54e929596a835fcbf6292310fd7
zakariya mohamed <zakariyakassim@live.com>
2018-08-21 16:31:52 +0100

    Initial Commit

---
+++ PortKiller/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PortKiller
+//
+//  Created by zakariya mohamed on 21/08/2018.
+//  Copyright © 2018 Zakaria Mohamed. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PortKiller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PortKiller
+//
+//  Created by zakariya mohamed on 21/08/2018.
+//  Copyright © 2018 Zakaria Mohamed. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
